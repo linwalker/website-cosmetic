3213aa29b147ce670fe7fadab39fb98b22b594a0
linwalker <wzamate@163.com>
2017-07-24 18:38:44 +0800

    banner

--- platforms/app/containers/Home/Home.js
+++ platforms/app/containers/Home/Home.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react'
-import {connect} from 'react-redux';
+import { connect } from 'react-redux';
 import { bindActionCreators } from 'redux';
 import { Carousel } from 'antd';
 import * as serverActions from '../../common/actions/serverState';
@@ -7,46 +7,58 @@ import * as serverActions from '../../common/actions/serverState';
 import './Home.css';
 
 function mapStateToProps(state) {
-    return {
-        serverConfig: state.server.serverConfig,
-        loaded: state.server.loaded
-    }
+  return {
+    serverConfig: state.server.serverConfig,
+    loaded: state.server.loaded
+  }
 }
 
 function mapDispatchToProps(dispatch) {
-    return bindActionCreators(serverActions, dispatch)
+  return bindActionCreators(serverActions, dispatch)
 }
 
-class Home extends Component{
-  constructor(){
+class Home extends Component {
+  constructor() {
     super()
   }
 
-  componentDidMount () {
-   const { loaded} = this.props
-   if ( !loaded ) {
+  componentDidMount() {
+    const { loaded } = this.props
+    if (!loaded) {
       this.props.fetchServerStateIfNeeded(this.props)
-   }
- }
-  
+    }
+  }
 
-  render(){
-    const {id="", username="", mobile="", email="", county="" } = this.props.serverConfig || {};
+
+  render() {
+    const { id = "", username = "", mobile = "", email = "", county = "" } = this.props.serverConfig || {};
     console.log('render props:', this.props)
     return (
       <div>
         <Carousel autoplay>
           <div >
-            <img className="bannerImg" src="http://www.yuesai.com/Content/Upload/Banner/20170719200626sskna354.jpg" alt=""/>
+            <picture>
+              <source srcSet="http://www.yuesai.com/Content/Upload/Banner/20170719200727g4gbigut.jpg" media="(max-width: 900px)" />
+              <img className="bannerImg" srcSet="http://www.yuesai.com/Content/Upload/Banner/20170719200626sskna354.jpg" alt="pic" />
+            </picture>
           </div>
           <div >
-            <img className="bannerImg" src="http://www.yuesai.com/Content/Upload/Banner/20170719200222pvsqnavs.jpg" alt=""/>
+            <picture>
+              <source srcSet="http://www.yuesai.com/Content/Upload/Banner/20170719200547y0njixi1.jpeg" media="(max-width: 900px)" />
+              <img className="bannerImg" srcSet="http://www.yuesai.com/Content/Upload/Banner/20170719200222pvsqnavs.jpg" alt="pic" />
+            </picture>
           </div>
           <div >
-            <img className="bannerImg" src="http://www.yuesai.com/Content/Upload/Banner/20170703144956rjni53ae.jpg" alt=""/>
+            <picture>
+              <source srcSet="http://www.yuesai.com/Content/Upload/Banner/201707031450204xuqyg5g.jpg" media="(max-width: 900px)" />
+              <img className="bannerImg" srcSet="http://www.yuesai.com/Content/Upload/Banner/20170703144956rjni53ae.jpg" alt="pic" />
+            </picture>
           </div>
-          <div >
-            <img className="bannerImg" src="http://www.yuesai.com/Content/Upload/Banner/2017030811451324cm4cyx.jpg" alt=""/>
+          <div>
+            <picture>
+              <source srcSet="http://www.yuesai.com/Content/Upload/Banner/20170103163830zfewcrov.jpg" media="(max-width: 900px)" />
+              <img className="bannerImg" srcSet="http://www.yuesai.com/Content/Upload/Banner/2017030811451324cm4cyx.jpg" alt="pic" />
+            </picture>
           </div>
         </Carousel>
         <div>
@@ -63,4 +75,4 @@ class Home extends Component{
 }
 
 
-export default  connect(mapStateToProps, mapDispatchToProps)(Home);
+export default connect(mapStateToProps, mapDispatchToProps)(Home);
